055a4d0bc7f69a13b135951bf279240822a0ad97
THEORMOVE <theormove@gmail.com>
2021-01-05 03:18:17 +0200

    testim

--- planeks_test/settings.py
+++ planeks_test/settings.py
@@ -149,3 +149,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
